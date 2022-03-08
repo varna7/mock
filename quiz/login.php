5dcc0f3eca8a429b45ad91d740ba8bece6781a41
Varna7 <varna.7.me@gmail.com>
2022-03-08 11:48:26 +0530

    quiz

--- quiz/login.php
+++ quiz/login.php
@@ -28,11 +28,21 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 		if (mysqli_num_rows($result) === 1) {
 			$row = mysqli_fetch_assoc($result);
             if ($row['user_name'] === $uname && $row['password'] === $pass) {
-            	$_SESSION['user_name'] = $row['user_name'];
-            	$_SESSION['name'] = $row['name'];
-            	$_SESSION['id'] = $row['id'];
-            	header("Location: home.php");
-		        exit();
+				if($row['user_name']=="admin")
+				{
+					$_SESSION['user_name'] = $row['user_name'];
+            		$_SESSION['name'] = $row['name'];
+            		$_SESSION['id'] = $row['id'];
+            		header("Location: admin.php");
+		        	exit();
+				}
+				else{
+					$_SESSION['user_name'] = $row['user_name'];
+            		$_SESSION['name'] = $row['name'];
+            		$_SESSION['id'] = $row['id'];
+            		header("Location: home.php");
+		        	exit();
+				}
             }else{
 				header("Location: index.php?error=Incorect User name or password");
 		        exit();
